9273eda82e98f4f10a13bbecad8a43bdf85f4f65
Janis <janisk@ithouse.lv>
2012-11-24 13:06:18 +0200

    app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileEventGallery::Application.config.secret_token = 'be739834cfb41c517c4dd3d3262dba7ead54a584c884b5ec2975b9ab73e6a4b0c08d74fc908281af511a8e74f3403951dade12bb64ae48e956c3cbea414faa5b'
